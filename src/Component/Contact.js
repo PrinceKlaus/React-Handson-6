d175a1a1b0b27406650b94ea26475f21d99aedaf
PrinceKlaus <pkv202000@gmail.com>
2023-09-15 15:06:06 +0530

    second commit

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,11 +1,6 @@
 import React from 'react';
-// import axios from 'axios';
 
 function Contact(){
-    // const APIkey = 'b88f67779fea85dbcad19274ee0a2dfd';
-    // axios.get(`https://api.openweathermap.org/data/2.5/weather?q={Mumbai}&appid={APIkey}`)
-    // .then((response)=> console.log(response.data))
-    // .catch((err) => console.log(err))
     return(
         <>
         <h1>Hi this is Contact</h1>
